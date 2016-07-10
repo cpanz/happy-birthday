2f0bd8bda715d04e004245b2d6d1b2fe5c8472d6
Cheng Pan <cproripop@gmail.com>
2016-07-10 04:26:04 -0400

    Initial Commit

---
+++ app/src/main/java/me/cheng/happybirthday/MainActivity.java
@@ -0,0 +1,13 @@
+package me.cheng.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(me.cheng.happybirthday.R.layout.activity_main);
+    }
+}
